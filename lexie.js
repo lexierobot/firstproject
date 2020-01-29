407de8dad0692f6c6bf47adf7985c511e607dbab
lexiehelwig <plumlexie@gmail.com>
2020-01-29 16:11:27 -0500

    Javascript homework

---
+++ lexie.js
@@ -0,0 +1,21 @@
+// Lexie's javascript homework
+
+"Frodo" + "Gollum"
+
+var Frodo = "good";
+var Gollum = "bad";
+
+console.log (Frodo + Gollum);
+
+var shire = "home";
+var mordor = 666;
+
+console.log (shire * mordor);
+console.log (2 + true);
+
+var a = "1";
+var b = "2";
+
+console.log(a + b);
+console.log (a * b);
+console.log ((a * b) + (2 + true))
