ce7fef0ddf036c60a088723f3a3cbc425e9bfd56
lexiehelwig <plumlexie@gmail.com>
2020-02-03 13:59:43 -0500

    Arrays homework

---
+++ arrays.js
@@ -0,0 +1,32 @@
+   //EXERCISE ARRAY
+
+   //1 10 grocery store items on loop, log each
+   var groceries = [ 
+       "bananas",
+       "spinach",
+       "almond milk",
+       "peanut butter",
+       "avocado",
+       "butter",
+       "la croix",
+       "blueberries",
+       "strawberries",
+       "yogurt"
+    ];
+    var i;
+  for (i = 0; i <= 9; i++){
+      console.log(groceries[i]);
+  }
+
+//2 string 10char+, log each character
+var str = "peanut butter";
+var i = str.charAt();
+for (i = 0; i <=12; i++) {
+    console.log(str[i]);
+}
+
+//3 Make the string uppercase
+var j = str.toUpperCase();
+console.log(j)
+
+
