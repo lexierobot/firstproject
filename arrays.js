83d3d4ec9634f70c3f380a146fd92aeb2be718d0
lexiehelwig <plumlexie@gmail.com>
2020-02-03 16:04:49 -0500

    Homeowrk update

--- arrays.js
+++ arrays.js
@@ -29,4 +29,24 @@ for (i = 0; i <=12; i++) {
 var j = str.toUpperCase();
 console.log(j)
 
+//EXERCISE ARRAYS 2: Revenge of the Index
 
+var inventory = [];
+
+function fillArray() {
+    inventory["bananas"] = 50,
+    inventory["spinach"] = 40,
+    inventory["peanut butter"] = 34,
+    inventory["avocado"] = 28,
+    inventory["almond milk"] = 50,
+    inventory["potatoes"] = 30,
+    inventory["butter"] = 100,
+    inventory["chicken"] = 50,
+    inventory["cheese"] = 100,
+    inventory["bread"] = 50,
+fillArray();
+inventory.forEach(buy)
+    function buy(item, index, inventory) {
+    inventory[index] = item - 1;
+}
+}
