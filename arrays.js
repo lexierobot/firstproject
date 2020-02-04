030afec3416f415a386db81d6756a3e6a4cc0618
lexiehelwig <plumlexie@gmail.com>
2020-02-04 16:36:07 -0500

    Array homework

--- arrays.js
+++ arrays.js
@@ -1,53 +1,98 @@
-   //EXERCISE ARRAY
+//    //EXERCISE ARRAY
 
-   //1 10 grocery store items on loop, log each
-   var groceries = [ 
-       "bananas",
-       "spinach",
-       "almond milk",
-       "peanut butter",
-       "avocado",
-       "butter",
-       "la croix",
-       "blueberries",
-       "strawberries",
-       "yogurt"
-    ];
-    var i;
-  for (i = 0; i <= 9; i++){
-      console.log(groceries[i]);
-  }
+//    //1 10 grocery store items on loop, log each
+//    var groceries = [ 
+//        "bananas",
+//        "spinach",
+//        "almond milk",
+//        "peanut butter",
+//        "avocado",
+//        "butter",
+//        "la croix",
+//        "blueberries",
+//        "strawberries",
+//        "yogurt"
+//     ];
+//     var i;
+//   for (i = 0; i <= 9; i++){
+//       console.log(groceries[i]);
+//   }
 
-//2 string 10char+, log each character
-var str = "peanut butter";
-var i = str.charAt();
-for (i = 0; i <=12; i++) {
-    console.log(str[i]);
-}
+// //2 string 10char+, log each character
+// var str = "peanut butter";
+// var i = str.charAt();
+// for (i = 0; i <=12; i++) {
+//     console.log(str[i]);
+// }
 
-//3 Make the string uppercase
-var j = str.toUpperCase();
-console.log(j)
+// //3 Make the string uppercase
+// var j = str.toUpperCase();
+// console.log(j)
 
 //EXERCISE ARRAYS 2: Revenge of the Index
+//Grocery Store App
     
-var inventory = [
-    ['bananas', 100],
-    ['spinach', 99],
-    ['peanut butter', 50],
-    ['avocado', 49],
-    ['almond milk', 100],
-    ['potatoes', 99],
-    ['butter', 50],
-    ['chicken', 49],
-    ['cheese', 100]
-    ['bread', 99]
-]
-    function add(){
-    var input = document.getElementById("input").value;
+// var inventory = [
+//     ['bananas', 100],
+//     ['spinach', 99],
+//     ['peanut butter', 50],
+//     ['avocado', 49],
+//     ['almond milk', 100],
+//     ['potatoes', 99],
+//     ['butter', 50],
+//     ['chicken', 49],
+//     ['cheese', 100]
+//     ['bread', 99]
+// ]
+//     function add(){
+//     var input = document.getElementById('user input').value;
 
-    for (var i = 0; i <= inventory.length[1]; i++) {
-        if(inventory[index] == input)
-            console.log(inventory[i--]);
+//     for (var i = 0; i <= inventory.length; i++) {
+//         if(input == array[0]){
+//             add.push(inventory[0] -=1);
+//         }
+//     }
+// } add();
+
+// option 1: multideminsional array
+let inventory = [
+    ['apple', 10],
+    ['blueberry', 15],
+    ['broccoli', 5],
+    ['milk', 25],
+    ['cheese', 4]
+]
+
+let cart = [];
+
+function add(){
+    let input = document.getElementById('userInput').value;
+    inventory.forEach(array => {
+        let name = array[0];
+        let qty = array[1];
+        if(input == name) {
+            array[1] +=1;
+            let temp = [];
+            temp.push(name);
+            temp.push(qty);
+            cart.push(temp);
+            console.log(cart);
+            console.log(inventory);
+        }    
+    });
+}
+
+function sub (){
+    let input = document.getElementById('userInput').value;
+    for(let i = 0; i < inventory.length; i++) {
+        if(input == inventory[i][0]) {
+            if(inventory[i][1] > 0) {
+                inventory[i][1] -= 1;
+                console.log(inventory[i]);
+            } else {
+                console.log(`${input} is out of stock!!`);
+            }  
+
+        }
     }
-} add();
+}
