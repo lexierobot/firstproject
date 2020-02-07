c9a1b6e7f35dc6abef26a0b83ac6a7711624185b
lexiehelwig <plumlexie@gmail.com>
2020-02-07 15:46:48 -0500

    More rewriting of old homework

--- arrays.js
+++ arrays.js
@@ -1,33 +1,33 @@
-   //EXERCISE ARRAY
+//    //EXERCISE ARRAY
 
-   //1 10 grocery store items on loop, log each
-   var groceries = [ 
-       "bananas",
-       "spinach",
-       "almond milk",
-       "peanut butter",
-       "avocado",
-       "butter",
-       "la croix",
-       "blueberries",
-       "strawberries",
-       "yogurt"
-    ];
-    var i;
-  for (i = 0; i <= 9; i++){
-      console.log(groceries[i]);
-  }
+//    //1 10 grocery store items on loop, log each
+//    var groceries = [ 
+//        "bananas",
+//        "spinach",
+//        "almond milk",
+//        "peanut butter",
+//        "avocado",
+//        "butter",
+//        "la croix",
+//        "blueberries",
+//        "strawberries",
+//        "yogurt"
+//     ];
+//     var i;
+//   for (i = 0; i <= 9; i++){
+//       console.log(groceries[i]);
+//   }
 
-//2 string 10char+, log each character
-var str = "peanut butter";
-var i = str.charAt();
-for (i = 0; i <=12; i++) {
-    console.log(str[i]);
-}
+// //2 string 10char+, log each character
+// var str = "peanut butter";
+// var i = str.charAt();
+// for (i = 0; i <=12; i++) {
+//     console.log(str[i]);
+// }
 
-//3 Make the string uppercase
-var j = str.toUpperCase();
-console.log(j)
+// //3 Make the string uppercase
+// var j = str.toUpperCase();
+// console.log(j)
 
 // //EXERCISE ARRAYS 2: Revenge of the Index
 // //Grocery Store App
@@ -54,48 +54,48 @@ console.log(j)
 //     }
 // } add();
 
-// option 1: multideminsional array
-let inventory = [
-    ['apple', 10],
-    ['blueberry', 15],
-    ['broccoli', 5],
-    ['milk', 25],
-    ['cheese', 4]
-]
+// // option 1: multideminsional array
+// let inventory = [
+//     ['apple', 10],
+//     ['blueberry', 15],
+//     ['broccoli', 5],
+//     ['milk', 25],
+//     ['cheese', 4]
+// ]
 
-let cart = [];
+// let cart = [];
 
-function add(){
-    let input = document.getElementById('userInput').value;
-    inventory.forEach(array => {
-        let name = array[0];
-        let qty = array[1];
-        if(input == name) {
-            array[1] +=1;
-            let temp = [];
-            temp.push(name);
-            temp.push(qty);
-            cart.push(temp);
-            console.log(cart);
-            console.log(inventory);
-        }    
-    });
-}
+// function add(){
+//     let input = document.getElementById('userInput').value;
+//     inventory.forEach(array => {
+//         let name = array[0];
+//         let qty = array[1];
+//         if(input == name) {
+//             array[1] +=1;
+//             let temp = [];
+//             temp.push(name);
+//             temp.push(qty);
+//             cart.push(temp);
+//             console.log(cart);
+//             console.log(inventory);
+//         }    
+//     });
+// }
 
-function sub (){
-    let input = document.getElementById('userInput').value;
-    for(let i = 0; i < inventory.length; i++) {
-        if(input == inventory[i][0]) {
-            if(inventory[i][1] > 0) {
-                inventory[i][1] -= 1;
-                console.log(inventory[i]);
-            } else {
-                console.log(`${input} is out of stock!`);
-            }  
+// function sub (){
+//     let input = document.getElementById('userInput').value;
+//     for(let i = 0; i < inventory.length; i++) {
+//         if(input == inventory[i][0]) {
+//             if(inventory[i][1] > 0) {
+//                 inventory[i][1] -= 1;
+//                 console.log(inventory[i]);
+//             } else {
+//                 console.log(`${input} is out of stock!`);
+//             }  
 
-        }
-    }
-}
+//         }
+//     }
+// }
 
 //Exercise Array
 //Rewrite
@@ -126,6 +126,55 @@ for (i = 0; i <= x.length; i++) {
 };
 
 //3
-for (i = 0; i <= x.length; i++) {
-    console.log(x.toUpperCase([i]));
-};
+console.log(x.toUpperCase([i]));
+
+//Exercise Arrays 2: Revenge of the Index
+
+var grocList = [
+    {item: 'bananas', qty: 100},
+    {item: 'spinach', qty: 99},
+    {item: 'milk', qty: 50},
+    {item: 'honey', qty: 100},
+    {item: 'butter', qty: 20},
+    {item: 'sugar', qty: 30},
+    {item: 'flour', qty: 100},
+    {item: 'juice', qty: 65},
+    {item: 'orange', qty: 78},
+    {item: 'tomato', qty: 45},
+];
+
+//Subtract 1 from each inventory item.
+grocList.forEach(function(grocList) {
+    console.log('-1 ' + grocList.item + ', New qty = '+ (grocList.qty - 1));
+});
+
+//Exercise Grocery Store App
+
+var grocStore = [
+    {invItem: 'peaches', invQty: 24},
+    {invItem: 'pineapple', invQty: 75},
+    {invItem: 'peanut butter', invQty: 46},
+    {invItem: 'yogurt', invQty: 28},
+    {invItem: 'broccoli', invQty: 23},
+    {invItem: 'potatoes', invQty: 54},
+    {invItem: 'oatmeal', invQty: 91}
+];
+
+let input = document.getElementById('userInput').value;
+    // if (function add()) {
+    //     console.log();
+
+    // } else if function sub() {
+
+    // } else func
+
+
+
+
+
+
+
+
+
+
+
